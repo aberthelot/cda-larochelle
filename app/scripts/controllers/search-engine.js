4fae3998818d1f044eff3a10cf290291882bb8b2
Alvin Berthelot <aberthelot@ippon.fr>
2013-10-02 14:57:19 +0200

    controller search-engine

---
+++ app/scripts/controllers/search-engine.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('cdaLarochelleApp')
+  .controller('SearchEngineCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
