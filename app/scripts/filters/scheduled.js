1227bfe19347a21f68dcb9b2cde5f89f881ac1a5
Alvin Berthelot <aberthelot@ippon.fr>
2013-10-09 18:42:25 +0200

    comparaison dynamique des dates mais avec des erreurs de décalage

--- app/scripts/filters/scheduled.js
+++ app/scripts/filters/scheduled.js
@@ -2,15 +2,21 @@
 
 angular.module('cdaLarochelleApp')
   .filter('scheduled', function () {
-    return function (items) {
+    return function (items, day) {
 
-    	var arrayToReturn = [];
+      // console.log('TEST :: ' + day);
+
+      var arrayToReturn = [];
 
         for (var i=0; i<items.length; i++){
             // if (items[i].title == 'Test B') {
             // console.log('TEST :: ' + items[i].scheduledBegin.);
-            if (moment(items[i].scheduledBegin).isAfter('2013-10-08')
-            	&& moment(items[i].scheduledEnd).isBefore('2013-10-20')) {
+
+            // if (moment(items[i].scheduledBegin).isAfter('2013-10-08')
+            //   && moment(items[i].scheduledEnd).isBefore('2013-10-20')) {
+
+            if (moment(day).isAfter(items[i].scheduledBegin)
+              && moment(day).isBefore(items[i].scheduledEnd)) {
                 arrayToReturn.push(items[i]);
             }
         }
