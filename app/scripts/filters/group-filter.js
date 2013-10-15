bbc2a1433f3bf5a64ec4f80d08b31c3b156d033b
Alvin Berthelot <aberthelot@ippon.fr>
2013-10-15 18:16:32 +0200

    mise en place du group by par source

--- app/scripts/filters/group-filter.js
+++ app/scripts/filters/group-filter.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('cdaLarochelleApp')
-  .filter('groupFilter', function () {
+  .filter('sourceFilter', function () {
     return function (items, sources) {
 
       var selectedSources = [];
@@ -15,6 +15,30 @@ angular.module('cdaLarochelleApp')
       var arrayToReturn = [];
       for (var i=0; i<items.length; i++) {
         if (selectedSources.indexOf(items[i].source) != -1) {
+          arrayToReturn.push(items[i]);
+        }
+      }
+      console.log(arrayToReturn.length + ' result(s) found');
+
+      return arrayToReturn;
+    };
+  });
+
+angular.module('cdaLarochelleApp')
+  .filter('groupFilter', function () {
+    return function (items, name) {
+
+      // var selectedSources = [];
+      // for (var i = 0; i < sources.length; i++) {
+      //   if (sources[i].selected) {
+      //     selectedSources.push(sources[i].name);
+      //   }
+      // };
+      // console.log('Selected sources : ' + selectedSources);
+
+      var arrayToReturn = [];
+      for (var i=0; i<items.length; i++) {
+        if (name == items[i].source) {
           arrayToReturn.push(items[i]);
         }
       }
