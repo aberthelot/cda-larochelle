163475ed692dcc76ac386e577f6a8c94301a8863
Alvin Berthelot <aberthelot@ippon.fr>
2013-10-15 11:37:13 +0200

    améliorations graphiques + mock json revu

--- app/scripts/controllers/bo-cms.js
+++ app/scripts/controllers/bo-cms.js
@@ -21,24 +21,34 @@ angular.module('cdaLarochelleApp')
 
   $rootScope.$on('dropEvent', function(evt, id, day, scheduled) {
 
-      console.log('Actualité #' + id + ' déposée le ' + day);
+      // console.log('Actualité #' + id + ' déposée le ' + day);
 
       for (var i = 0; i < $scope.topics.length; i++) {
         if ($scope.topics[i].id === id) {
           if ('begin' === scheduled) {
-            console.log(scheduled);
-            $scope.topics[i].scheduledBegin = day;
-            if ($scope.topics[i].scheduledEnd == null || $scope.topics[i].scheduledEnd == "") {
-              $scope.topics[i].scheduledEnd = day;
+            // console.log(scheduled);
+            if (moment(day).isAfter($scope.topics[i].scheduledEnd)) {
+              alert('Impossible de mettre une date de début après une date de fin');
+            } else {
+              $scope.topics[i].scheduledBegin = day;
+              if ($scope.topics[i].scheduledEnd == null || $scope.topics[i].scheduledEnd == "") {
+                $scope.topics[i].scheduledEnd = day;
+              }
             }
           } else {
-            console.log(scheduled);
-            $scope.topics[i].scheduledEnd = day;
-            if ($scope.topics[i].scheduledBegin == null || $scope.topics[i].scheduledBegin == "") {
-              $scope.topics[i].scheduledBegin = day;
+
+            if (moment(day).isBefore($scope.topics[i].scheduledBegin)) {
+              alert('Impossible de mettre une date de fin après une date de début');
+            } else {
+              $scope.topics[i].scheduledEnd = day;
+              if ($scope.topics[i].scheduledBegin == null || $scope.topics[i].scheduledBegin == "") {
+                $scope.topics[i].scheduledBegin = day;
+              }
             }
+            // console.log(scheduled);
+
           }
-        $scope.topics[i].isScheduledEnd = true;
+        $scope.topics[i].isScheduled = true;
         }
       };
 
