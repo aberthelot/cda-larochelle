1bd931f386e217af154f501bce293c6ba3cb2bed
Alvin Berthelot <aberthelot@ippon.fr>
2013-10-15 12:10:47 +0200

    correction de l'effet lumineux pour dropper un article

--- app/scripts/directives/draganddrop.js
+++ app/scripts/directives/draganddrop.js
@@ -2,51 +2,28 @@
 
 var draganddropUI = angular.module('draganddropUI', []);
 
-
-
-// eventUI.directive('minitopic', function() {
-//   return {
-//     restrict: 'E',
-//     replace: true,
-//     template:
-
-//       '<article'
-//       + ' data-category-id="' + category_id +'">'
-//         + '<p>'
-//           + '<span class="label label-category-'
-//           + category_id +'">'
-//             + category_title
-//           + '</span>'
-//         + '</p>'
-//         + '<p>' + title + '</p>'
-//       // evt.target.innerHTML += '<p> <button>test</button>';
-//         + '</article>';
-
-
-//     '<div class="tile">' +
-//     '<h3>{{event.title}}</h3>' +
-//     '<img src="{{event.picture}}" alt="{{event.title}}">' +
-//     '<p>{{event.teasing}}</p>' +
-//     '<a class="btn btn-primary btn-large" href="http://designmodo.com/flat">' +
-//     '<i class="icon-chevron-right icon-white"></i><i class="icon-chevron-right icon-white"></i> Inscription</a>' +
-//     '</div>'
-//   }
-// });
-
 draganddropUI.directive('drag', ["$rootScope", function($rootScope) {
 
   function dragStart(evt, element, dragStyle) {
     element.addClass(dragStyle);
+
+    var id = evt.target.parentNode.getAttribute('data-id');
+    var title = evt.target.parentNode.getAttribute('data-title');
+    var category_id = evt.target.parentNode.getAttribute('data-category-id');
+    var category_title = evt.target.parentNode.getAttribute('data-category-title');
+    var scheduled = evt.target.getAttribute('data-scheduled');
+
+
     /**
     on ne peut pas utiliser directement evt.dataTransfer à cause jQuery ...
     **/
     // informations propres à l'actualité
-    evt.originalEvent.dataTransfer.setData('data-id', evt.target.parentNode.getAttribute('data-id'));
-    evt.originalEvent.dataTransfer.setData('data-title', evt.target.parentNode.getAttribute('data-title'));
-    evt.originalEvent.dataTransfer.setData('data-category-id', evt.target.parentNode.getAttribute('data-category-id'));
-    evt.originalEvent.dataTransfer.setData('data-category-title', evt.target.parentNode.getAttribute('data-category-title'));
+    evt.originalEvent.dataTransfer.setData('data-id', id);
+    evt.originalEvent.dataTransfer.setData('data-title', title);
+    evt.originalEvent.dataTransfer.setData('data-category-id', category_id);
+    evt.originalEvent.dataTransfer.setData('data-category-title', category_title);
     // informations propres à la date
-    evt.originalEvent.dataTransfer.setData('data-scheduled', evt.target.getAttribute('data-scheduled'));    
+    evt.originalEvent.dataTransfer.setData('data-scheduled', scheduled);    
 
 
     // evt.originalEvent.dataTransfer.setData('data-dateFirst', evt.target.getAttribute('data-dateFirst'));
@@ -54,10 +31,12 @@ draganddropUI.directive('drag', ["$rootScope", function($rootScope) {
 
     // on mémorise l'identifiant de la catégorie pour les actions autres que drop
     // car il n'est pas possible d'utiliser dataTransfer hormis dans drop
-    localStorage.setItem('category_id_dragged', evt.target.getAttribute('data-category-id'));
+    localStorage.setItem('id_dragged', id);
+    localStorage.setItem('category_id_dragged', category_id);
   };
   function dragEnd(evt, element, dragStyle) {
     element.removeClass(dragStyle);
+    localStorage.removeItem('id_dragged');
     localStorage.removeItem('category_id_dragged');
   };
 
@@ -83,10 +62,15 @@ draganddropUI.directive('drop', function($rootScope) {
   function dragEnter(evt, element, dropStyle) {
     // evt.preventDefault();
 
+    var id = localStorage.getItem('id_dragged');
+    // console.log('ID : ' + id);
     var category_id = localStorage.getItem('category_id_dragged');
+    // console.log('CATEGORY_ID : ' + category_id);
     var droppableElement = getDroppableElement(evt);
 
-    if (checkAvailableCategory(droppableElement, category_id)) {
+    // console.log('Vérification du drop : ' + checkAvailableCategory(droppableElement, id, category_id));
+
+    if (checkAvailableCategory(droppableElement, id, category_id)) {
       element.addClass(dropStyle + '-success');
     } else {
       element.addClass(dropStyle + '-failure');
@@ -125,33 +109,11 @@ draganddropUI.directive('drop', function($rootScope) {
 
 
     if (checkAvailableCategory(droppableElement, id, category_id)) {
-      // // evt.target.innerHTML += '<p>' + evt.originalEvent.dataTransfer.getData('data-id') + '</p>';
-      // droppableElement.innerHTML += '<article'
-      // // + ' draggable="true"'
-      // // + ' drag'
-      // + ' data-category-id="' + category_id +'">'
-      //   + '<p>'
-      //     + '<span class="label label-category-'
-      //     + category_id +'">'
-      //       + category_title
-      //     + '</span>'
-      //   + '</p>'
-      //   + '<p>' + title + '</p>'
-      // // evt.target.innerHTML += '<p> <button>test</button>';
-      //   + '</article>';
-
-    // on récupère la date
-    var dropDay = droppableElement.querySelectorAll('time')[0].getAttribute('datetime');
-    // 
-    $rootScope.$broadcast('dropEvent', id, dropDay, scheduled);
+      // on récupère la date
+      var dropDay = droppableElement.querySelectorAll('time')[0].getAttribute('datetime');
+      // 
+      $rootScope.$broadcast('dropEvent', id, dropDay, scheduled);
     }
-
-
-
-
-
-
-
 
   };
   
@@ -206,8 +168,9 @@ function checkAvailableCategory(droppableElement, id, category_id) {
     // console.log('NB ARTICLES :: ' + droppedArticles.length);
 
     for (var i = 0; i < droppedArticles.length; i++) {
-      // alert('TEST :: ' + droppedArticles[i].innerHTML);
       // console.log(droppedArticles[i].getAttribute('data-category-id') + ' =?= ' + category_id);
+      // console.log(droppedArticles[i].getAttribute('data-id') + ' =?= ' + id);
+
       if (droppedArticles[i].getAttribute('data-category-id') == category_id
         && droppedArticles[i].getAttribute('data-id') !== id) {
         check = false;
